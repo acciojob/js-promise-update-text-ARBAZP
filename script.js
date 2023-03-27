c8ee4d864f2f93cb9482f57c70767275d1bea131
ARBAZP <117530037+ARBAZP@users.noreply.github.com>
2023-03-27 14:23:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateText() {
+	return new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+	resolve("Hello, world!");
+},1000);
+
+});
+}
+
+updateText().then((result)=>{
+	const output = document.getElementById("output");
+	output.innerText = result;
+})
